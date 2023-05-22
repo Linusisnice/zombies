0cfcd9bf5f5adbbcf6d5cee56c72c92c918e410f
Linusisnice <97303140+Linusisnice@users.noreply.github.com>
2023-05-22 18:29:25 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,6 +4,8 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -49,7 +51,7 @@ namespace myTiles {
 ........................................
 ........................................
 ........................................
-`, [myTiles.transparency16,sprites.builtin.brick,myTiles.tile1], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.builtin.brick,myTiles.tile1,myTiles.tile2], TileScale.Sixteen);
         }
         return null;
     })
@@ -59,6 +61,8 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "myTile":
             case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
         }
         return null;
     })
