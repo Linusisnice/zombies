650c35d1c0e3b6140fa5edff224e3cff91e162b7
Linusisnice <97303140+Linusisnice@users.noreply.github.com>
2023-05-22 18:08:51 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+tiles.setCurrentTilemap(tilemap`level1`)
 let Hero = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -16,8 +17,8 @@ let Hero = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(Hero)
 scene.cameraFollowSprite(Hero)
+controller.moveSprite(Hero)
 forever(function () {
     if (controller.right.isPressed()) {
         Hero.setImage(img`
